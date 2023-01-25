ea8f15ec35d51d9c9dd82a5c7f81d290a36321ef
TOWER-PC\A_Alh <a_alhelal@hotmail.com>
2023-01-25 21:31:19 +0100

    Add mymodule

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-const http = require('http');
-const url = require('url');
+
+const mymodule = require('./mymodule');
+mymodule.getData('https://www.example.com');
+
+
+// // SERVER
+// const http = require('http');
+// const url = require('url');
+// const server = http.createServer((req, res) => {
+//   const pathname = req.url;
+
+//   // Home page
+//   if (pathname === '/' || pathname === '/home') {
+//     // mymodule.getData('https://www.example.com')
+//     // res.end('This is HOME');
 
 
 
-// SERVER
-const server = http.createServer((req, res) => {
-  const pathname = req.url;
+//     res.end(   mymodule.getData('https://www.example.com')  );
 
-  // Home page
-  if (pathname === '/' || pathname === '/home') {
-    res.end('This is HOME');
 
-    // Services page
-  } else if (pathname === '/services') {
-    res.end('This is Services');
+       
+//   } else if (pathname === '/services') {
+//     res.end('This is Services');
 
-    // Not found
-  } else {
-    res.writeHead(404, {
-      'Content-type': 'text/html',
-      'My-header': 'Hello!'
-    });
-    res.end('<h1>Page not found!</h1>');
-  }
-});
+//     // Not found
+//   } else {
+//     res.writeHead(404, {
+//       'Content-type': 'text/html',
+//       'My-header': 'Hello!'
+//     });
+//     res.end('<h1>Page not found!</h1>');
+//   }
+// });
 
-server.listen(7000, '127.0.0.1', () => {
-  console.log('Listening on port 7000'); 
-});
+// server.listen(7000, '127.0.0.1', () => {
+//   console.log('Listening on port 7000'); 
+// });
